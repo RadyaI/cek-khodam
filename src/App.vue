591bbd17edb6f9713eccb41a85ed41a8a57f2ffe
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-06-27 13:46:56 +0700

    Fixing Responsive

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 }
 
 body {
-  height: 100dvh;
+  height: 100vh;
   background-image: radial-gradient(circle at center center, transparent 0%, rgb(33, 33, 33) 99%), repeating-linear-gradient(272deg, rgba(163, 163, 163, 0.2) 0px, rgba(163, 163, 163, 0.2) 1px, transparent 1px, transparent 6px), repeating-linear-gradient(362deg, rgba(163, 163, 163, 0.2) 0px, rgba(163, 163, 163, 0.2) 1px, transparent 1px, transparent 6px), linear-gradient(362deg, rgb(33, 33, 33), rgb(33, 33, 33));
 }
 </style>
